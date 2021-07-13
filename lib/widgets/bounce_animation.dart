95b3dffe8573400930bbe1542b2552009afabee0
Varun Ramnani <varunhhhrahul@gmail.com>
2021-07-14 01:01:13 +0530

    cleaned bounce animation

--- lib/widgets/bounce_animation.dart
+++ lib/widgets/bounce_animation.dart
@@ -4,14 +4,16 @@ import 'package:flutter/material.dart';
 class BounceAnimation extends StatelessWidget {
   final Widget child;
   final VoidCallback onPress;
-  const BounceAnimation({required this.child, required this.onPress});
+  //
+  const BounceAnimation({Key? key, required this.child, required this.onPress})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return BouncingWidget(
       duration: const Duration(milliseconds: 100),
       scaleFactor: 0.5,
-      onPressed: onPress != null ? onPress : () {},
+      onPressed: onPress ?? () => {},
       child: child,
     );
   }
